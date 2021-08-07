821f26cbab7d0e0a2e791a6a92b363ae27303797
webgenapp <87999663+webgenapp@users.noreply.github.com>
2021-08-07 08:43:13 +0200

    Create app using webgen.dev

---
+++ migrations/2021876438-create-shovel.js
@@ -0,0 +1,28 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Shovels', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      size: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Shovels')
+  },
+}
